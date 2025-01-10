07acfe92ea43bab3e64925293d213cadcb1296b0
angiehelensanchez <angiehelensanchez@gmail.com>
2025-01-10 10:04:37 +0100

    feet: add letter seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Letter;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +13,96 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $letter = new Letter();
+        $letter->letter = 'T';
+        $letter->save();
+        
+        $letter = new Letter();
+        $letter->letter = 'R';
+        $letter->save();
+        
+        $letter = new Letter();
+        $letter->letter = 'W';
+        $letter->save();
+        
+        $letter = new Letter();
+        $letter->letter = 'A';
+        $letter->save();
+        
+        $letter = new Letter();
+        $letter->letter = 'G';
+        $letter->save();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $letter = new Letter();
+        $letter->letter = 'M';
+        $letter->save();
+
+        $letter = new Letter();
+        $letter->letter = 'Y';
+        $letter->save();
+
+        $letter = new Letter();
+        $letter->letter = 'F';
+        $letter->save();
+        
+        $letter = new Letter();
+        $letter->letter = 'P';
+        $letter->save();
+
+        $letter = new Letter();
+        $letter->letter = 'D';
+        $letter->save();
+
+        $letter = new Letter();
+        $letter->letter = 'X';
+        $letter->save();
+
+        $letter = new Letter();
+        $letter->letter = 'B';
+        $letter->save();
+
+        $letter = new Letter();
+        $letter->letter = 'N';
+        $letter->save();
+
+        $letter = new Letter();
+        $letter->letter = 'J';
+        $letter->save();
+
+        $letter = new Letter();
+        $letter->letter = 'Z';
+        $letter->save();
+
+        $letter = new Letter();
+        $letter->letter = 'S';
+        $letter->save();
+
+        $letter = new Letter();
+        $letter->letter = 'Q';
+        $letter->save();
+
+        $letter = new Letter();
+        $letter->letter = 'V';
+        $letter->save();
+
+        $letter = new Letter();
+        $letter->letter = 'H';
+        $letter->save();
+
+        $letter = new Letter();
+        $letter->letter = 'L';
+        $letter->save();
+
+        $letter = new Letter();
+        $letter->letter = 'C';
+        $letter->save();
+
+        $letter = new Letter();
+        $letter->letter = 'K';
+        $letter->save();
+
+        $letter = new Letter();
+        $letter->letter = 'E';
+        $letter->save();
     }
 }
